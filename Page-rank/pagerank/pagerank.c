b07599ad8b4a67814a7c4a48cfa93e2f6ba49a12
VarunVk <vijay094@umn.edu>
2017-04-04 15:03:22 -0500

    Changes done for initial framework.
    Reading read for separately for each rank is done.

--- Page-rank/pagerank/pagerank.c
+++ Page-rank/pagerank/pagerank.c
@@ -2,10 +2,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <mpi.h>
+#include <string.h>
 
 #include "pr_graph.h"
 
 
+/* GLOBAL DECLARATIONS */
+#define ROOT 0
+int numThreads;
+int myrank;
+pr_graph * graph; 
 
 /**
 * @brief Compute the PageRank (PR) of a graph.
@@ -17,7 +24,7 @@
 * @return A vector of PR values.
 */
 double * pagerank(
-    pr_graph const * const graph,
+    pr_graph * graph,
     double const damping,
     int const max_iterations);
 
@@ -31,97 +38,231 @@ int main(
     return EXIT_FAILURE;
   }
 
+  /* MPI Initializations */
+  MPI_Init(NULL,NULL);
+  MPI_Comm_size(MPI_COMM_WORLD, &numThreads);
+  MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
+
+
   char * ifname = argv[1];
-  char * ofname = NULL;
-  if(argc > 2) {
-    ofname = argv[2];
-  }
 
-  pr_graph * graph = pr_graph_load(ifname);
+  graph = pr_graph_load(ifname);
   if(!graph) {
-    return EXIT_FAILURE;
-  }
-
-  double * PR = pagerank(graph, 0.85, 100);
-
-  /* write pagerank values */
-  if(ofname) {
-    FILE * fout = fopen(ofname, "w");
-    if(!fout) {
-      fprintf(stderr, "ERROR: could not open '%s' for writing.\n", ofname);
       return EXIT_FAILURE;
-    }
-    for(pr_int v=0; v < graph->nvtxs; ++v) {
-      fprintf(fout, "%0.3e\n", PR[v]);
-    }
-    fclose(fout);
   }
 
-  free(PR);
+  double * PR = pagerank(graph, 0.85, 100); 
 
+#if 0
+  if(myrank==ROOT) {
+      printf("Writing to output file .\n");
+      char * ofname = NULL;
+      if(argc > 2) {
+          ofname = argv[2];
+      }
+      /* write pagerank values */
+      if(ofname) {
+          FILE * fout = fopen(ofname, "w");
+          if(!fout) {
+              fprintf(stderr, "ERROR: could not open '%s' for writing.\n", ofname);
+              return EXIT_FAILURE;
+          }
+          for(pr_int v=0; v < graph->nvtxs; ++v) {
+              fprintf(fout, "%0.3e\n", PR[v]);
+          }
+          fclose(fout);
+      }
+      free(PR);
+  }
+#endif
+  MPI_Finalize();
   return EXIT_SUCCESS;
 }
 
 
 
 double * pagerank(
-    pr_graph const * const graph,
+    pr_graph *Rgraph,
     double const damping,
     int const max_iterations)
 {
-  /* grab graph structures to save typing */
-  pr_int const nvtxs = graph->nvtxs;
-  pr_int const * const restrict xadj = graph->xadj;
-  pr_int const * const restrict nbrs = graph->nbrs;
+  pr_graph * Lgraph = malloc(sizeof(*graph));
+  memset(Lgraph, 0, sizeof(*graph));
+  pr_int tot_nvtxs;
+
+  if(myrank==ROOT) 
+      tot_nvtxs=Rgraph->nvtxs;
+  MPI_Bcast(&tot_nvtxs, 1,MPI_UINT64_T, ROOT, MPI_COMM_WORLD);
+  Lgraph->nvtxs=tot_nvtxs/numThreads;
+
+
+  // Scatter the adj data 
+  Lgraph->xadj = malloc((Lgraph->nvtxs+1)*sizeof(*Lgraph->xadj));
+  if(Lgraph->xadj==NULL)
+  {
+      printf("Unable to allocate mem for xadj");
+      return NULL;
+  }
+  memset(Lgraph->xadj, 0, (Lgraph->nvtxs+1)*sizeof(*Lgraph->xadj));
+
+  // Scatter the xadj to all ranks 
+  int sendcounts[numThreads];
+  int displs[numThreads];
+  for(int i=0; i<numThreads; i++)
+  {
+      sendcounts[i]=Lgraph->nvtxs+1;
+      displs[i]=(i*Lgraph->nvtxs);
+  }
+  void *send;
+  if(myrank==ROOT)
+      send=Rgraph->xadj;
+  else 
+      send=NULL;
+
+  if(MPI_SUCCESS != MPI_Scatterv(send, sendcounts, displs, MPI_UINT64_T, 
+                                Lgraph->xadj, Lgraph->nvtxs+1, MPI_UINT64_T,
+                                ROOT, MPI_COMM_WORLD)) {
+      printf("Scattering the xadj failed.!!!\n");
+      return NULL;
+  }
+  // Time to scatter nbrs to all ranks 
+  int nnbrs=(Lgraph->xadj[Lgraph->nvtxs]-Lgraph->xadj[0])/*-1 Should we subtract one here*/;
+  Lgraph->nbrs = malloc(nnbrs*sizeof(*Lgraph->nbrs));
+  if(Lgraph->nbrs==NULL)
+  {
+      printf("Unable to allocate mem for nbrs");
+      return NULL;
+  }
+  memset(Lgraph->nbrs, 0, nnbrs * sizeof(*Lgraph->nbrs));
+
+
+  if(myrank==ROOT){
+      for(int i=0; i<numThreads; i++)
+      {
+          sendcounts[i]=Rgraph->xadj[(i+1)*Lgraph->nvtxs]- Rgraph->xadj[i*Lgraph->nvtxs];
+          displs[i]=Rgraph->xadj[i*Lgraph->nvtxs];
+      }
+  }
+
+  if(myrank==ROOT)
+      send=Rgraph->nbrs;
+  else 
+      send=NULL;
+  if(MPI_SUCCESS != MPI_Scatterv(send, sendcounts, displs, MPI_UINT64_T, 
+                                Lgraph->nbrs, nnbrs, MPI_UINT64_T,
+                                ROOT, MPI_COMM_WORLD)) {
+      printf("Scattering the xadj failed.!!!\n");
+      return NULL;
+  }
 
   /* Initialize pageranks to be a probability distribution. */
-  double * PR = malloc(nvtxs * sizeof(*PR));
-  for(pr_int v=0; v < nvtxs; ++v) {
-    PR[v] = 1. / (double) nvtxs;
+  double * PR = malloc(Lgraph->nvtxs * sizeof(*PR));
+  for(pr_int v=0; v < Lgraph->nvtxs; ++v) {
+    PR[v] = 1. / (double) tot_nvtxs;
   }
 
   /* Probability of restart */
-  double const restart = (1 - damping) / (double) nvtxs;
+  double const restart = (1 - damping) / (double) tot_nvtxs;
 
 
   /* Convergence tolerance. */
   double const tol = 1e-12;
 
-  double * PR_accum = malloc(nvtxs * sizeof(*PR));
-  for(int i=0; i < max_iterations; ++i) {
+  double * PR_accum = malloc(Lgraph->nvtxs * sizeof(*PR));
 
-    for(pr_int v=0; v < nvtxs; ++v) {
+  /* Setup MPI win for Inter process communication*/
+  MPI_Win win;
+
+  /* int MPI_Win_create( void *base, MPI_Aint size, int disp_unit, MPI_Info info, MPI_Comm comm, MPI_Win *win); */
+  MPI_Win_create(PR_accum, Lgraph->nvtxs * sizeof(*PR), sizeof(*PR), MPI_INFO_NULL, MPI_COMM_WORLD, &win);
+
+  /* int MPI_Win_fence( int assert, MPI_Win win); */
+  MPI_Win_fence(0, win);
+
+  pr_int out_nnbrs=0;
+  pr_int out_nbrs_loc[nnbrs];
+  double out_nbrs_val[nnbrs];
+  memset(out_nbrs_loc, 0, nnbrs*sizeof(pr_int));
+
+  /* Main loop */
+  for(int i=0; i < 1 /*max_iterations*/; ++i) {
+      memset(out_nbrs_val, 0, nnbrs*sizeof(double));
+
+    for(pr_int v=0; v < Lgraph->nvtxs; ++v) {
       PR_accum[v] = 0.;
     }
 
     /* Each vertex pushes PR contribution to all outgoing links */
-    for(pr_int v=0; v < nvtxs; ++v) {
-      double const num_links = (double)(xadj[v+1] - xadj[v]);
+    for(pr_int v=0; v < Lgraph->nvtxs; ++v) {
+      double const num_links = (double)(Lgraph->xadj[v+1] - Lgraph->xadj[v]);
       double const pushing_val = PR[v] / num_links;
 
-      for(pr_int e=xadj[v]; e < xadj[v+1]; ++e) {
-        PR_accum[nbrs[e]] += pushing_val;
+      for(pr_int e=Lgraph->xadj[v]; e < Lgraph->xadj[v+1]; ++e) {
+          // Check if the nbr is in our rank 
+          if((Lgraph->nbrs[e]> myrank*Lgraph->nvtxs) && (Lgraph->nbrs[e]<(myrank+1)*Lgraph->nvtxs))
+              PR_accum[Lgraph->nbrs[e]] += pushing_val;
+          else { 
+              continue;
+              int dup=0;
+              for(pr_int x=0; x<out_nnbrs; x++)
+              {
+                  if(out_nbrs_loc[x]==Lgraph->nbrs[e]) {
+                      out_nbrs_val[x] += pushing_val;
+                      out_nnbrs++;
+                      dup=1;
+                      break;
+                  }
+              }
+              if(dup==0) {
+                  out_nbrs_loc[out_nnbrs] = Lgraph->nbrs[e];
+                  out_nbrs_val[out_nnbrs] += pushing_val;
+                  out_nnbrs++;
+              }
+
+          }
       }
     }
+    /* Send out the Pushing_val to the outside nbrs */
 
+    for(pr_int x=0; x<out_nnbrs; x++)
+    {
+        // MPI Unicast the value to the resp rank 
+        int target_rank = (int )(out_nbrs_loc[x]/Lgraph->nvtxs); 
+        int target_disp = (out_nbrs_loc[x]%Lgraph->nvtxs);
+        MPI_Accumulate(&out_nbrs_val[x], 1, MPI_DOUBLE, target_rank, target_disp, 1, MPI_DOUBLE, MPI_SUM, win);
+        MPI_Win_fence(0, win);
+    }
     /* Finalize new PR values */
     double norm_changed = 0.;
-    for(pr_int v=0; v < nvtxs; ++v) {
+    for(pr_int v=0; v < Lgraph->nvtxs; ++v) {
       double const old = PR[v];
       PR[v] = restart + (damping * PR_accum[v]);
 
-      norm_changed = (PR[v] - old) * (PR[v] - old);
+      norm_changed += (PR[v] - old) * (PR[v] - old);
     }
     norm_changed = sqrt(norm_changed);
+
+    // For MPI, lets get the max(norm_changed) from all ranks and check if it less than tol
+    // then terminate the processing .
+    MPI_Allreduce(MPI_IN_PLACE, &norm_changed, 1, MPI_DOUBLE, MPI_MAX, MPI_COMM_WORLD);
 
     if(i > 1 && norm_changed < tol) {
       break;
     }
+    printf("%d. %f\n", myrank, norm_changed);
+    MPI_Barrier(MPI_COMM_WORLD);
   }
 
-  free(PR_accum);
-  return PR;
+  //free(PR_accum);
+  /* Only on ROOT gather all the PR values. */
+  double *G_PR;
+  if(myrank == ROOT) {
+        G_PR=malloc(Rgraph->nvtxs*sizeof(*PR));
+  }
+  MPI_Gather(PR, Lgraph->nvtxs, MPI_DOUBLE, G_PR, Lgraph->nvtxs, MPI_DOUBLE, ROOT, MPI_COMM_WORLD);
+  //free(PR);
+  MPI_Win_free(&win);
+  printf("%d I'm done. \n", myrank);
+  MPI_Barrier(MPI_COMM_WORLD);
+  return G_PR;
 }
-
-
-
--- Page-rank/pagerank/pr_graph.c
+++ Page-rank/pagerank/pr_graph.c
@@ -10,80 +10,145 @@
 #include <stdlib.h>
 #include <string.h>
 #include <assert.h>
+#include <mpi.h>
 
 #include "pr_graph.h"
 
-
+#define ROOT 0
+extern int myrank;
+int numThreads;
 
 pr_graph * pr_graph_load(
     char const * const ifname)
 {
-  FILE * fin = fopen(ifname, "r");
-  if(!fin) {
-    fprintf(stderr, "ERROR: could not open '%s' for reading.\n", ifname);
-    return NULL;
-  }
+    pr_graph * graph;
+    FILE * fin; 
+    if(myrank==ROOT) {
 
-  pr_graph * graph = malloc(sizeof(*graph));
+        fin = fopen(ifname, "r");
+        if(!fin) {
+            fprintf(stderr, "ERROR: could not open '%s' for reading.\n", ifname);
+            return NULL;
+        }
 
-  /* read nvtxs and nedges */
-  fscanf(fin, "%lu", &(graph->nvtxs));
-  fscanf(fin, "%lu", &(graph->nedges));
-  fscanf(fin, "\n"); /* make sure we process the newline, too. */
+        graph = malloc(sizeof(*graph));
 
-  graph->xadj = malloc((graph->nvtxs + 1) * sizeof(*graph->xadj));
-  graph->nbrs = malloc(graph->nedges * sizeof(*graph->nbrs));
-
-  /* How many edges we have read. */
-  pr_int edge_ptr = 0;
-
-  char * line = malloc(1024 * 1024);
-  size_t len = 0;
-
-  /* Read in graph one vertex at a time. */
-  for(pr_int v=0; v < graph->nvtxs; ++v) {
-    ssize_t read = getline(&line, &len, fin);
-    if(read == -1) {
-      fprintf(stderr, "ERROR: premature EOF at line %lu\n", v+1);
-      pr_graph_free(graph);
-      return NULL;
+        /* read nvtxs and nedges */
+        fscanf(fin, "%lu", &(graph->nvtxs));
+        fscanf(fin, "%lu", &(graph->nedges));
+        fscanf(fin, "\n"); /* make sure we process the newline, too. */
     }
 
-    /* Store the beginning of the adjacency list. */
-    graph->xadj[v] = edge_ptr;
+    pr_graph * Lgraph = malloc(sizeof(*Lgraph));
+    memset(Lgraph, 0, sizeof(*Lgraph));
+    pr_int tot_nvtxs;
 
-    /* Check for sinks -- these make pagerank more difficult. */
-    if(read == 1) {
-      fprintf(stderr, "WARNING: vertex '%lu' is a sink vertex.\n", v+1);
-      continue;
+    if(myrank==ROOT) 
+        tot_nvtxs=graph->nvtxs;
+
+    MPI_Bcast(&tot_nvtxs, 1,MPI_UINT64_T, ROOT, MPI_COMM_WORLD);
+
+    Lgraph->nvtxs=tot_nvtxs/numThreads;
+    printf("(%d): Everyone gets %lu.\n", myrank, Lgraph->nvtxs);
+
+    if(myrank==ROOT) {
+        graph->xadj = malloc((Lgraph->nvtxs + 1) * sizeof(*graph->xadj));
+        graph->nbrs = malloc(graph->nedges * sizeof(*graph->nbrs));
+        if(graph->nbrs==NULL)
+            printf("Error while allocating mem for graph->nbrs on ROOT.\n");
     }
+    Lgraph->xadj = malloc((Lgraph->nvtxs + 1) * sizeof(*Lgraph->xadj));
 
-    /* Foreach edge in line. */
-    char * ptr = strtok(line, " ");
-    while(ptr != NULL) {
-      char * end = NULL;
-      pr_int const e_id = strtoull(ptr, &end, 10);
-      /* end of line */
-      if(ptr == end) {
-        break;
-      }
-      assert(e_id > 0 && e_id <= graph->nvtxs);
+    char * line = malloc(1024 * 1024);
+    size_t len = 0;
 
-      graph->nbrs[edge_ptr++] = e_id - 1; /* 1 indexed */
-      ptr = strtok(NULL, " ");
+    if(myrank==ROOT)
+    {
+        for(int x=0; x<numThreads ;x++)
+        {
+#if 1
+            pr_int v;
+            /* How many edges we have read. */
+            pr_int edge_ptr = 0;
+
+            /* Read in graph one vertex at a time. */
+            for(v=0; v < Lgraph->nvtxs; ++v) {
+                ssize_t read = getline(&line, &len, fin);
+                if(read == -1) {
+                    fprintf(stderr, "ERROR: premature EOF at line %lu\n", v+1);
+                    pr_graph_free(graph);
+                    return NULL;
+                }
+
+                /* Store the beginning of the adjacency list. */
+                graph->xadj[v] = edge_ptr;
+
+                /* Check for sinks -- these make pagerank more difficult. */
+                if(read == 1) {
+                    fprintf(stderr, "WARNING: vertex '%lu' is a sink vertex.\n", v+1);
+                    continue;
+                }
+
+                /* Foreach edge in line. */
+                char * ptr = strtok(line, " ");
+                while(ptr != NULL) {
+                    char * end = NULL;
+                    pr_int const e_id = strtoull(ptr, &end, 10);
+                    /* end of line */
+                    if(ptr == end) {
+                        break;
+                    }
+                    assert(e_id > 0 && e_id <= graph->nvtxs);
+
+                    graph->nbrs[edge_ptr++] = e_id - 1; /* 1 indexed */
+                    ptr = strtok(NULL, " ");
+                }
+            }
+            //assert(edge_ptr == graph->nedges);
+            graph->xadj[v] = edge_ptr;
+#endif 
+
+            MPI_Request send_req;
+            MPI_Status   status;
+            MPI_Isend(graph->xadj, Lgraph->nvtxs+1, MPI_UINT64_T, x, x, MPI_COMM_WORLD, &send_req); 
+            /* Send edge_ptr and then the actual data */
+            MPI_Wait(&send_req, &status);
+            printf("edge_ptr %lu == nvtxs %lu\n", edge_ptr, graph->xadj[Lgraph->nvtxs]);
+
+            MPI_Isend(graph->nbrs, edge_ptr, MPI_UINT64_T, x, 2*x, MPI_COMM_WORLD, &send_req); 
+            MPI_Wait(&send_req, &status);
+        } 
+        /* MPI_Wait should come here  */
+    } 
+    {
+        MPI_Request recv_req;
+        MPI_Status   status;
+        /* int MPI_Irecv(void *buf, int count, MPI_Datatype datatype, int source,
+           int tag, MPI_Comm comm, MPI_Request *request) */ 
+        MPI_Irecv(Lgraph->xadj, Lgraph->nvtxs+1,MPI_UINT64_T, ROOT, myrank, MPI_COMM_WORLD, &recv_req); 
+        MPI_Wait(&recv_req, &status);
+        printf("(%d): Recd %lu xadj values.\n", myrank, Lgraph->nvtxs);
+        for(pr_int y=0; y<Lgraph->nvtxs+1; y++)
+            printf("(%d): xadj[%lu]=%lu .\t", myrank,y, Lgraph->xadj[y]);
+        printf("\n");
+
+        Lgraph->nbrs=malloc(Lgraph->xadj[Lgraph->nvtxs]*sizeof(*Lgraph->nbrs));
+        MPI_Irecv(Lgraph->nbrs, Lgraph->xadj[Lgraph->nvtxs]+1,MPI_UINT64_T, ROOT, 2*myrank, MPI_COMM_WORLD, &recv_req); 
+        MPI_Wait(&recv_req, &status);
+
+        printf("(%d): Recd %lu nbr values.\n", myrank, Lgraph->nvtxs);
+        for(pr_int y=0; y<Lgraph->xadj[Lgraph->nvtxs]; y++)
+            printf("(%d): nbrs[%lu]=%lu .\t", myrank,y, Lgraph->nbrs[y]);
+        printf("\n");
     }
-  }
-  assert(edge_ptr == graph->nedges);
-  graph->xadj[graph->nvtxs] = graph->nedges;
-
-  free(line);
-
-  return graph;
+    pr_graph_free(graph);
+    free(line);
+    return Lgraph;
 }
 
 
 void pr_graph_free(
-    pr_graph * const graph)
+        pr_graph * const graph)
 {
   free(graph->xadj);
   free(graph->nbrs);
